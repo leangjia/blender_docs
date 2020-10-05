54db04b36270365de46bf3073bd833b437b1d197
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2020-10-05 16:29:55 +0000

    CSS: Split css for version switch to its own file
    This makes the css easier to navigate.
    The goal is to eventually refactor this more

--- manual/conf.py
+++ manual/conf.py
@@ -227,6 +227,7 @@ html_static_path = ["../resources/theme"]
 def setup(app):
     if html_theme == "sphinx_rtd_theme":
         app.add_css_file("css/theme_overrides.css")
+        app.add_css_file("css/version_switch.css")
         app.add_js_file("js/version_switch.js")
 
 # Add any extra paths that contain custom files (such as robots.txt or
