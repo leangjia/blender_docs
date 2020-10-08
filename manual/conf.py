ed63d3c3709b50e96573569ae67006e80613213e
campbellbarton <campbellbarton@c4de1f47-6596-e411-a384-0024e86c2797>
2020-10-08 01:03:16 +0000

    Support building without 'sphinx_rtd_theme'
    It seems this is no longer part of a default sphinx installation,
    so support building without it.

--- manual/conf.py
+++ manual/conf.py
@@ -176,7 +176,13 @@ gettext_compact = False     # optional.
 # -- Options for HTML output ---------------------------------------------------
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+html_theme = "default"
+try:
+    __import__("sphinx_rtd_theme")
+    html_theme = "sphinx_rtd_theme"
+except ModuleNotFound:
+    pass
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
